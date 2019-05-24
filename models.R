e43b6bf5996107fb03524a492ec6472370da8a6f
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-05-24 11:33:17 +0100

    Update models.R

--- models.R
+++ models.R
@@ -170,6 +170,68 @@
         model_g <- stepAIC(glm(xg$out_gld~.,data = xg[,-1],family = binomial(link = "logit")))
         
         
-  
+   d=  glm(formula = xd$out_dlr ~ activeAddresses + paymentCount + blockSize + 
+              txVolume.USD. + marketcap.USD. + price.USD. + medianFee.1 + 
+              realizedCap.USD..2 + generatedCoins.2 + blockCount.2 + txCount.3 + 
+              generatedCoins.3 + fees.3 + txVolume.USD..3 + txCount.4 + 
+              fees.4 + activeAddresses.4 + averageDifficulty.4 + medianTxValue.USD..3 + 
+              blockCount.4 + txVolume.USD..4 + txCount.5 + marketcap.USD..4 + 
+              price.USD..4 + exchangeVolume.USD..4 + activeAddresses.5 + 
+              txVolume.USD..5 + marketcap.USD..5 + price.USD..5 + activeAddresses.6 + 
+              averageDifficulty.5 + paymentCount.5 + txCount.7 + price.USD..6 + 
+              medianTxValue.USD..6 + x....1. + txVolume.USD..7 + txCount.8 + 
+              exchangeVolume.USD..7 + medianFee.6 + paymentCount.6 + marketcap.USD..8 + 
+              price.USD..8 + realizedCap.USD..3 + generatedCoins.6 + paymentCount.7 + 
+              blockSize.6 + blockCount.6 + exchangeVolume.USD..9 + txCount.11 + 
+              marketcap.USD..10 + price.USD..10 + exchangeVolume.USD..10 + 
+              realizedCap.USD..4 + blockCount.7 + price.USD..11 + activeAddresses.11 + 
+              medianTxValue.USD..10 + x....1..1 + txVolume.USD..12 + reward + 
+              txCount.14 + price.USD..13 + realizedCap.USD..5 + generatedCoins.8 + 
+              activeAddresses.12 + paymentCount.9 + medianTxValue.USD..11 + 
+              blockCount.8 + txCount.15 + marketcap.USD..14 + price.USD..14 + 
+              medianFee.10 + activeAddresses.13 + txCount.16 + marketcap.USD..15 + 
+              price.USD..15 + exchangeVolume.USD..15 + fees.11 + medianTxValue.USD..13 + 
+              medianFee.11 + txCount.17 + marketcap.USD..16 + price.USD..16 + 
+              exchangeVolume.USD..17 + generatedCoins.9 + activeAddresses.16 + 
+              averageDifficulty.9 + paymentCount.10 + blockCount.9 + txCount.19 + 
+              paymentCount.11 + activeAddresses.17 + medianFee.13 + price.USD..18 + 
+              txVolume.USD..18 + adjustedTxVolume.USD..8 + marketcap.USD..19 + 
+              generatedCoins.10 + averageDifficulty.10 + paymentCount.12 + 
+              medianTxValue.USD..15 + medianFee.14 + blockSize.10 + blockCount.10, 
+            family = binomial(link = "logit"), data = xd[, -1])
+        
+        
+        
+        
+        go<-  glm(formula = xg$out_gld ~ txCount + generatedCoins + activeAddresses + 
+                     medianFee + blockSize + blockCount + txVolume.USD. + marketcap.USD. + 
+                     price.USD. + activeAddresses.1 + paymentCount.1 + medianTxValue.USD. + 
+                     txVolume.USD..1 + adjustedTxVolume.USD..1 + realizedCap.USD..2 + 
+                     generatedCoins.2 + fees.2 + paymentCount.2 + medianTxValue.USD..1 + 
+                     blockCount.2 + txVolume.USD..2 + averageDifficulty.3 + paymentCount.3 + 
+                     blockCount.3 + txCount.4 + exchangeVolume.USD..3 + generatedCoins.4 + 
+                     fees.4 + activeAddresses.4 + medianFee.4 + txCount.5 + marketcap.USD..4 + 
+                     medianTxValue.USD..4 + medianFee.5 + averageDifficulty.5 + 
+                     paymentCount.5 + marketcap.USD..5 + price.USD..5 + exchangeVolume.USD..5 + 
+                     generatedCoins.6 + medianFee.6 + averageDifficulty.6 + blockSize.6 + 
+                     x....1..1 + txCount.7 + price.USD..6 + fees.7 + paymentCount.7 + 
+                     txVolume.USD..7 + txCount.8 + activeAddresses.8 + averageDifficulty.7 + 
+                     paymentCount.8 + medianTxValue.USD..7 + txVolume.USD..8 + 
+                     txCount.9 + marketcap.USD..8 + txVolume.USD..9 + adjustedTxVolume.USD..6 + 
+                     txCount.10 + exchangeVolume.USD..9 + averageDifficulty.8 + 
+                     paymentCount.9 + blockCount.8 + price.USD..10 + exchangeVolume.USD..10 + 
+                     medianTxValue.USD..9 + x....1..2 + txCount.12 + txCount.13 + 
+                     marketcap.USD..12 + price.USD..12 + exchangeVolume.USD..12 + 
+                     generatedCoins.9 + fees.10 + averageDifficulty.9 + paymentCount.10 + 
+                     medianFee.10 + blockSize.9 + blockCount.9 + txCount.14 + 
+                     marketcap.USD..13 + price.USD..13 + fees.11 + medianFee.11 + 
+                     txVolume.USD..14 + txCount.16 + price.USD..15 + txCount.17 + 
+                     exchangeVolume.USD..16 + fees.14 + blockSize.10 + exchangeVolume.USD..17 + 
+                     adjustedTxVolume.USD..8 + marketcap.USD..18 + realizedCap.USD..6 + 
+                     fees.16 + activeAddresses.17 + paymentCount.13 + blockSize.11, 
+                   family = binomial(link = "logit"), data = xg[, -1])
+
+
+
 
 
